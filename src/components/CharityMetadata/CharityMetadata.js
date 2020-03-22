a435099f17b398398b5a7f46cbb8cbc575789575
James Woo <jamesjaywoo@gmail.com>
2020-03-22 15:00:03 -0400

    Add charity meta data component

---
+++ src/components/CharityMetadata/CharityMetadata.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Button, Divider, Icon } from 'semantic-ui-react';
+import './CharityMetadata.scss';
+
+export default function CharityMetadata() {
+  return (
+    <div className="charity-metadata">
+      <h3>Video title</h3>
+      <div className="charity-stats">
+        <span>Mar 19, 2020</span>
+        <div className="charity-actions">
+          <Button basic icon labelPosition="left">
+            <Icon name="heart" />
+            Follow
+          </Button>
+          <Button basic icon labelPosition="left">
+            <Icon name="share" />
+            Share
+          </Button>
+        </div>
+      </div>
+      <Divider />
+    </div>
+  );
+}
--- src/containers/Charity/Charity.js
+++ src/containers/Charity/Charity.js
@@ -2,17 +2,13 @@ import React from 'react';
 import Video from '../../components/Video/Video';
 import RelatedCharities from '../../components/RelatedCharities/RelatedCharities';
 import './Charity.scss';
+import CharityMetadata from '../../components/CharityMetadata/CharityMetadata';
 
 export default function Charity() {
   return (
     <div className="charity-detail-grid">
       <Video className="video" id="396392696" title="breast friends" />
-      <div
-        className="metadata"
-        style={{ width: '100%', height: '100px', background: '#F91112' }}
-      >
-        Metadata
-      </div>
+      <CharityMetadata className="metadata" />
       <div
         className="charity-info-box"
         style={{ width: '100%', height: '100px', background: '#BD10E0' }}
