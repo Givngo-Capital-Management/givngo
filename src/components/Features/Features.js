8fd5a36b078ed9fb82f5db79a10620bb27f5cf83
Jack Capombassis <jcapombassis@gmail.com>
2020-03-29 16:27:29 -0400

    changed body tag to react fragment:

--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -9,12 +9,12 @@ export default function Features(){
   const images = ["https://givngo.io/wp-content/themes/givngo/images/OOOTTTIIIIII.png", "https://givngo.io/wp-content/themes/givngo/images/2305-6-Eblast-One-Tree-Planted-One-Tree-Planted-2.jpg"]
   const image_1 = ["https://givngo.io/wp-content/themes/givngo/images/No-Color-Present-Tense.jpg", "https://givngo.io/wp-content/themes/givngo/images/Screen-Shot-2019-11-15-at-11.22.20.png"]
   return ( 
-    <body className="body">  
+    <>  
       <FeatureHeaderSection />  
       <FeatureTwoImage urls={images} position="right" srcset_param="https://givngo.io/wp-content/themes/givngo/images/OOOTTTIIIIII-p-500.png 500w, https://givngo.io/wp-content/themes/givngo/images/OOOTTTIIIIII-p-800.png 800w, https://givngo.io/wp-content/themes/givngo/images/OOOTTTIIIIII-p-1080.png 1080w, https://givngo.io/wp-content/themes/givngo/images/OOOTTTIIIIII.png 2560w" />
       <FeatureVideoText />
       <FeatureTwoImage urls={image_1} position="left" srcset_param="https://givngo.io/wp-content/themes/givngo/images/No-Color-Present-Tense-p-500.jpeg 500w, https://givngo.io/wp-content/themes/givngo/images/No-Color-Present-Tense.jpg 851w" />
       <FeatureFormSection />
-    </body>
+    </>
   );
 }
